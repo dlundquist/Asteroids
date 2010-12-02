c6aaf1350f4e1250fe3b1cc779f088305eef4e30
Chris Lundquist <rampantdurandal@gmail.com>
2010-12-02 15:19:59 -0800

    added the framework for the powerup sound effect

--- src/SoundEffect.java
+++ src/SoundEffect.java
@@ -9,6 +9,7 @@ public class SoundEffect {
 	private static final String LARGE_ASTEROID_DEATH_FILE = "large_asteroid_explosion.mp3";
 	private static final String SMALL_ASTEROID_DEATH_FILE = "small_asteroid_explosion.mp3";
 	private static final String PLAYER_DEATH_FILE = "explode.mp3";
+	private static final String POWER_UP_EFFECT_FILE = "powerup_effect.mp3";
 
 
 	// We only want to make one copy of each sound effect and play the
@@ -18,6 +19,7 @@ public class SoundEffect {
 	private static Sound largeAsteroidDeath;
 	private static Sound smallAsteroidDeath;
 	private static Sound playerDeath;
+	private static Sound powerUpEffect;
 
 	// Initializes our sound effects
 	public static void init(boolean isEnabled) {
@@ -29,6 +31,7 @@ public class SoundEffect {
 			largeAsteroidDeath = new Sound(LARGE_ASTEROID_DEATH_FILE);
 			smallAsteroidDeath = new Sound(SMALL_ASTEROID_DEATH_FILE);
 			playerDeath = new Sound(PLAYER_DEATH_FILE);
+			powerUpEffect = new Sound(POWER_UP_EFFECT_FILE);
 		}
 		else {
 			System.err.println("Sound Disabled");
@@ -48,6 +51,10 @@ public class SoundEffect {
 	// Returns our sound for when bullets hit things
 	static public Sound forBulletHit() {
 		return bulletHit;
+	}
+	
+	static public Sound forPowerUp() {
+		return powerUpEffect;
 	}
 
 	// Returns our sound for when Asteroids blow up / die
